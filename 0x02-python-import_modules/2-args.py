613c9f13f557e28c87068df798bb738b92801b1d
peelife1 <mbiakijepeter@gmail.com>
2023-01-25 02:38:32 -0800

    updated files

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
+def print_args(argv):
+    length = len(argv)
+    if (length == 1):
+        print("{0:d} arguments.".format(length - 1))
+    elif (length > 1):
+        if (length - 1 == 1):
+            print("{0:d} argument:".format(length - 1))
+        elif (length > 1):
+            print("{0:d} arguments:".format(length - 1))
+        for i in range(length - 1):
+            print("{0:d}: {1:s}".format(i + 1, argv[i + 1]))
 
-import sys
 
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-for i in range(count):
-     print("{}: {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == "__main__":
+    import sys
+    print_args(sys.argv)
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+def add_values(argv):
+    count = 0
+    for i in range(len(argv)):
+        count += 1
+    sum_val = 0
+    idx = 1
+    while count != 1:
+        sum_val += int(argv[idx])
+        idx += 1
+        count -= 1
+    print(sum_val)
+
+
+if __name__ == "__main__":
+    import sys
+    add_values(sys.argv)
